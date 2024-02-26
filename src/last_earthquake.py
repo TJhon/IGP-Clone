15ca4d7e69363560ae328463291bcc5c6abbadde
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-02-26 13:24:16 -0500

    fix-lastreport

--- src/last_earthquake.py
+++ src/last_earthquake.py
@@ -5,15 +5,8 @@ import re, json
 def last_report():
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Referer": "https://ultimosismo.igp.gob.pe/inicio",
-        "Sec-Fetch-Dest": "document",
         "Sec-Fetch-Mode": "navigate",
         "Sec-Fetch-Site": "same-origin",
-        "Upgrade-Insecure-Requests": "1",
     }
 
     response = requests.get("https://ultimosismo.igp.gob.pe/", headers=headers)
