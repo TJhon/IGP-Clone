52a27603402a99eb47d2aca3e2f560e8672d026a
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-02-29 16:39:41 -0500

    Update active.py

--- active.py
+++ active.py
@@ -10,6 +10,15 @@ options.binary_location = '/usr/bin/chromium-browser'
 driver = webdriver.Chrome(options=options)
 driver.get("https://ipg-clone.streamlit.app/")
 time.sleep(5)
-print("5 secs")
+print("5 secs - igp")
+print(driver.title)
+driver.quit()
+
+
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://imporatations-peru-calculator.streamlit.app/")
+time.sleep(5)
+print("5 secs - calculator")
 print(driver.title)
 driver.quit()
