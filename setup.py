1f88e8aa504bd84c7f6f53716627adefa39b9461
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-10-24 10:17:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     install_requires=["requests>=2.25.1", "pandas>=1.1.5", "openpyxl>=3.0.0", "pytz"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
